343ed65a59f428f8efcdf1721c5f4b5391f3936f
vivaansr <100041238+vivaansr@users.noreply.github.com>
2026-09-13 11:43:35 -0500

    Update HelloWorld.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -5,5 +5,13 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
 public class HelloWorld extends OpMode {
-    
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "World");
+    }
+
+    @Override
+    public void loop() {
+        
+    }
 }
